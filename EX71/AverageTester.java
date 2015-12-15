824cd3b7f014bed52fde90286140a5ef9abd4d6f
Alster100 <aleclowery@live.com>
2015-12-15 12:55:46 -0500

    Created by Coding Ground Client

---
+++ EX71/AverageTester.java
@@ -0,0 +1,8 @@
+public class AverageTester{
+
+     public static void main(String []args){
+     // Write code here to test the AverageFinder class
+     AverageFinder two = new AverageFinder(34,6);
+     system.out.println(two.average());
+     }
+}
