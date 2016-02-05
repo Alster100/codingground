7e8efbd7d0d67756f5807cd449ac5f5fd6c7f898
Alster100 <aleclowery@live.com>
2016-02-05 13:15:44 -0500

    Created by Coding Ground Client

---
+++ New Project/DefensivePlayer.java
@@ -0,0 +1,16 @@
+
+public class DefensivePlayer
+{
+    public DefensivePlayer()
+    {
+        
+    }
+    
+    public void Reaction(String QBAction)
+    {
+        System.out.println("Stay");
+    }
+    
+    
+    
+}
